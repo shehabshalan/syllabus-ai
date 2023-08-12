c1f79985deaf28c0bede973de891b0363495aa1f
shehabshalan <shehab.shalan@hotmail.com>
2023-08-12 20:11:07 +0200

    feat: Home page with syllabus search and results

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,120 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import ChapterCard from "@/components/ChapterCard";
+import { Sparkles } from "lucide-react";
+import { useEffect, useState } from "react";
+import CardSkeleton from "@/components/CardSkeleton";
+import { CHAPTER_FUNCTION } from "@/utils/openaiFunctions";
+import { Chapter } from "../../global";
+import { generateChapters } from "@/utils/openai";
 
 export default function Home() {
+  const [query, setQuery] = useState<string>("");
+  const [isQuerying, setIsQuerying] = useState<boolean>(false);
+  const [chapters, setChapters] = useState<Chapter[]>([]);
+  const [error, setError] = useState<boolean>(false);
+
+  const handleQuery = async () => {
+    setChapters([]);
+    setError(false);
+    setIsQuerying(true);
+
+    try {
+      const response = await generateChapters({
+        functionCall: CHAPTER_FUNCTION,
+        query,
+        task: "chapter",
+      });
+      setChapters(response?.chapters);
+      localStorage.setItem("chapters", JSON.stringify(response?.chapters));
+    } catch (e) {
+      console.log(e);
+      setError(true);
+    } finally {
+      setIsQuerying(false);
+    }
+  };
+
+  useEffect(() => {
+    const chapters = localStorage.getItem("chapters");
+    if (chapters) {
+      setChapters(JSON.parse(chapters));
+    }
+  }, []);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
+    <section className="container mt-12 gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[700px] mx-auto flex-col items-center justify-center gap-6">
+        <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl ">
+          Learn <span className="underline">anything</span> with SyllabusAI
+        </h1>
+        <p>
+          (5/<span className="font-bold ">5</span>) queries
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+        <div className="flex w-full items-center space-x-2">
+          <div className="relative flex-grow">
+            <Input
+              className="h-12 pr-12 "
+              type="text"
+              placeholder="Type here a topic you want to learn"
+              value={query}
+              disabled={isQuerying}
+              onChange={(e) => setQuery(e.target.value)}
             />
-          </a>
+            <Button
+              className="absolute top-0 right-0 h-full px-4 rounded-tl-none rounded-bl-none"
+              type="submit"
+              disabled={isQuerying}
+              onClick={handleQuery}
+            >
+              <Sparkles className="mr-2 h-4 w-4" /> Learn
+            </Button>
+          </div>
         </div>
+        <p className="text-sm text-muted-foreground">
+          Tip: Try topics like "Programming", "Philosophy", or "Music Theory"
+        </p>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      {isQuerying || chapters.length > 0 ? (
+        <section
+          id="features"
+          className="container space-y-6 bg-zinc-50 py-8 dark:bg-transparent md:py-12 lg:py-12 mt-12"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
+            {isQuerying ? (
+              <>
+                <CardSkeleton />
+                <CardSkeleton />
+                <CardSkeleton />
+              </>
+            ) : null}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            {chapters && chapters.length > 0
+              ? chapters.map((chapter) => (
+                  <ChapterCard key={chapter.slug} chapter={chapter} />
+                ))
+              : null}
+          </div>
+          <div className="mx-auto text-center md:max-w-[58rem]">
+            <p className="text-sm text-muted-foreground">
+              AI may suggest you to learn a module that you already know. You
+              can skip it.
+            </p>
+          </div>
+        </section>
+      ) : null}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      {/* error display */}
+      {error ? (
+        <div className="flex flex-col items-center justify-center mt-12">
+          <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl mb-4 ">
+            No results found
+          </h1>
+        </div>
+      ) : null}
+    </section>
+  );
 }
