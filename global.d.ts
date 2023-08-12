2c315bb7274d7ec6ea4d248119dde2da64b5732b
shehabshalan <shehab.shalan@hotmail.com>
2023-08-12 20:09:32 +0200

    feat: Add types

---
+++ global.d.ts
@@ -0,0 +1,29 @@
+import { ChatCompletionFunctions } from "openai";
+
+export interface NavItem {
+  title: string;
+  href?: string;
+  disabled?: boolean;
+  external?: boolean;
+}
+
+export interface Task {
+  name: string;
+  slug: string;
+}
+
+export interface Chapter {
+  name: string;
+  slug: string;
+  description: string;
+}
+
+export interface CallOpenAIWithFunctionParams {
+  task: string;
+  query: string;
+  functionCall: ChatCompletionFunctions;
+}
+export interface CallOpenAIParams {
+  task: string;
+  query: string;
+}
