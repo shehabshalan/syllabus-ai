eb354d224fd34e665f4cf16ee90c8147941897a9
shehabshalan <shehab.shalan@hotmail.com>
2023-08-12 20:12:46 +0200

    feat: Define tasks for content

---
+++ src/utils/tasks.ts
@@ -0,0 +1,20 @@
+import { Task } from "../../global";
+
+export const tasks: Task[] = [
+  {
+    name: "Quiz",
+    slug: "quiz",
+  },
+  {
+    name: "Simplify",
+    slug: "simplify",
+  },
+  {
+    name: "Example",
+    slug: "example",
+  },
+  {
+    name: "Resources",
+    slug: "resources",
+  },
+];
