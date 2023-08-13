ea889eec4fff9cf41fe8f6a9260c36948ab59f9f
shehabshalan <shehab.shalan@hotmail.com>
2023-08-13 14:32:38 +0200

    temp: Debug production error

--- src/pages/api/generateLesson.ts
+++ src/pages/api/generateLesson.ts
@@ -19,6 +19,8 @@ export default async function handler(
       res.status(200).json(response);
     }
   } catch (error) {
+    console.log("error", error);
+
     res.status(500).json({ error });
   }
 }
