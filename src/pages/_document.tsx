26b254a1cc1fe7504bfadf5cbd7659e8602fbbf0
shehabshalan <shehab.shalan@hotmail.com>
2023-10-15 22:53:59 +0200

    chore: add next-18next lib and config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,27 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import type { DocumentProps } from "next/document";
+import i18nextConfig from "../../next-i18next.config";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <title>SyllabusAI - Your AI Tutor</title>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+type Props = DocumentProps & {
+  // add custom document props
+};
+
+class MyDocument extends Document<Props> {
+  render() {
+    const currentLocale =
+      this.props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
+    return (
+      <Html lang={currentLocale}>
+        <Head>
+          <title>SyllabusAI - Your AI Tutor</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
