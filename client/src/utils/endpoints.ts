f4731df5de4b0da39469ae06a913366167ceb6cc
Shehab Shalan <shehab.shalan@hotmail.com>
2024-09-27 22:29:37 +0200

    feat: add react

--- client/src/utils/endpoints.ts
+++
@@ -1,6 +0,0 @@
-export const ENDPOINTS = {
-  GENERATE_CHAPTERS: '/api/generate-chapters',
-  GENERATE_LESSON: '/api/generate-lesson',
-  GENERATE_QUIZ: '/api/generate-quiz',
-  SIMPLIFY_CHAPTER: '/api/simplify-chapter',
-};
