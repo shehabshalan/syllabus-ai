cb0cae9a072f0bb376d9935d9259c28a22994654
Shehab Shalan <shehab.shalan@hotmail.com>
2024-09-29 20:47:21 +0200

    chore: add openapi-code gen for client code generation

---
+++ client/openapi-codegen.config.ts
@@ -0,0 +1,24 @@
+import {
+  generateSchemaTypes,
+  generateReactQueryComponents,
+} from '@openapi-codegen/typescript';
+import { defineConfig } from '@openapi-codegen/cli';
+export default defineConfig({
+  api: {
+    from: {
+      relativePath: '../api/openapi.json',
+      source: 'file',
+    },
+    outputDir: 'src/api',
+    to: async (context) => {
+      const filenamePrefix = 'api';
+      const { schemasFiles } = await generateSchemaTypes(context, {
+        filenamePrefix,
+      });
+      await generateReactQueryComponents(context, {
+        filenamePrefix,
+        schemasFiles,
+      });
+    },
+  },
+});
