6e37b535964b1e0871ac007c012a8588e0e31cb3
Shehab Shalan <shehab.shalan@hotmail.com>
2024-10-05 15:17:48 +0200

    chore: fix google user check and jwt generation

--- api/app/utils/jwt.py
+++ api/app/utils/jwt.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta
 
+import requests
 from fastapi import HTTPException, status
-from google.auth.transport import requests
-from google.oauth2 import id_token
 from jose import JWTError, jwt
 
 from app.utils.settings import settings
@@ -17,6 +16,7 @@ def generate_access_token(data: dict):
             raise ValueError("No secret key configured for JWT")
 
         to_encode = data.copy()
+        print(to_encode)
         expire = datetime.now() + timedelta(hours=ACCESS_TOKEN_EXPIRE_MINUTES)
         to_encode.update({"exp": expire})
         encoded_jwt = jwt.encode(
@@ -24,19 +24,53 @@ def generate_access_token(data: dict):
         )
         return encoded_jwt
     except JWTError:
-        return None
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Failed to generate access token",
+        )
 
 
 def verify_google_token(token: str):
-    try:
-        if not settings.GOOGLE_CLIENT_ID:
-            raise ValueError("No client ID configured for Google OAuth")
-        id_info = id_token.verify_oauth2_token(
-            token, requests.Request(), settings.GOOGLE_CLIENT_ID
+    if not settings.GOOGLE_CLIENT_ID:
+        raise ValueError("No client ID configured for Google OAuth")
+
+    # Verify the token using Google's tokeninfo endpoint
+    response = requests.get(
+        f"https://oauth2.googleapis.com/tokeninfo?access_token={token}"
+    )
+
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
         )
-        return id_info
-    except ValueError:
-        return None
+
+    id_info = response.json()
+
+    # Fetch additional user info using the People API
+    user_info_response = requests.get(
+        "https://www.googleapis.com/oauth2/v1/userinfo", params={"access_token": token}
+    )
+
+    if user_info_response.status_code != 200:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Failed to fetch user info",
+        )
+
+    user_info = user_info_response.json()
+
+    # Combine the token info and user info
+    id_info.update(user_info)
+
+    user_info = {
+        "email": id_info["email"],
+        "name": id_info["name"],
+        "picture": id_info["picture"],
+        "id": id_info["id"],
+    }
+
+    return user_info
 
 
 def verify_token(token: str):
