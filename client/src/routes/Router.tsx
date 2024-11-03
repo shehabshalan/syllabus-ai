e92202d65c012766e6a1e59af34dd6f4c6118909
Shehab Shalan <shehab.shalan@hotmail.com>
2024-11-03 12:45:44 +0100

    chore: remove unused import

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,7 +1,6 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ROUTES } from './Routes';
-import ProtectedRoute from '@/ProtectedRoute';
 import NotFound from '@/pages/NotFound';
 import Loading from '@/components/ui/loading';
 
