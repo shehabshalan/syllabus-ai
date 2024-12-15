d31fe2190a879ed0d5fc61f5318cdf4526912617
Shehab Shalan <shehab.shalan@hotmail.com>
2024-12-15 01:46:00 +0100

    feat: enable chapter generation on chatper click

--- client/src/components/topic/TopicChapters.tsx
+++ client/src/components/topic/TopicChapters.tsx
@@ -8,12 +8,17 @@ import {
 } from '../ui/card';
 import { useNavigate } from 'react-router';
 import { Button } from '../ui/button';
+import { useGenerateChapter } from '@/api/apiHooks/llm-generation/llm-generation';
+import { useState } from 'react';
 
 type TopicChaptersProps = {
   chapters: Chapter[];
 };
 const TopicChapters = ({ chapters }: TopicChaptersProps) => {
   const navigate = useNavigate();
+  const { mutate: generateChatper, isPending } = useGenerateChapter();
+  const [activeCardId, setActiveCardId] = useState<number | null>(null);
+
   return (
     <>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 py-4 mt-4">
@@ -21,17 +26,41 @@ const TopicChapters = ({ chapters }: TopicChaptersProps) => {
           <Card key={index}>
             <CardHeader>
               <CardTitle>{chapter.title}</CardTitle>
-
               <CardDescription>{chapter.description}</CardDescription>
             </CardHeader>
             <CardFooter>
               <Button
                 variant="outline"
                 onClick={() => {
-                  navigate(`/chapter/${chapter.id}`);
+                  setActiveCardId(chapter.id);
+                  generateChatper(
+                    {
+                      data: {
+                        id: chapter.id,
+                        title: chapter.title,
+                        description: chapter.description,
+                      },
+                    },
+                    {
+                      onSuccess: (data) => {
+                        setTimeout(() => {
+                          navigate(`/chapter/${data.id}`);
+                          setActiveCardId(null);
+                        }, 1000);
+                      },
+                      onError: () => {
+                        console.error('Error generating chapter');
+                        setActiveCardId(null);
+                      },
+                    }
+                  );
                 }}
               >
-                View
+                {chapter.content
+                  ? 'View'
+                  : isPending && activeCardId === chapter.id
+                  ? 'Generating...'
+                  : 'Generate'}
               </Button>
             </CardFooter>
           </Card>
