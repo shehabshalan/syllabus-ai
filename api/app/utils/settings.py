b8e16368d8e7b30bdb4e707685303cdfef22e4f3
shehabshalan <shehab.shalan@hotmail.com>
2025-05-01 18:25:37 +0200

    chore: update .env.example and settings.py to include JWT algorithm and token expiration settings

--- api/app/utils/settings.py
+++ api/app/utils/settings.py
@@ -4,18 +4,25 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    # LLM config
+    GOOGLE_API_KEY: str
     OPENAI_API_KEY: str
     MODEL: str
+    # API config
     ORIGINS: list[str]
+    # JWT and Auth config
     GOOGLE_CLIENT_ID: str
     JWT_SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    # DB config
     DB_DRIVER: str
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
-    GOOGLE_API_KEY: str
+
     MODE: Literal["dev", "prod"] = "dev"
 
     class Config:
